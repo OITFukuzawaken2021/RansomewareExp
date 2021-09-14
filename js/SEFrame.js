fdcd89ba85abb431a3bd04b1540696eb33fa82df
e1n19026 <e1n19026@oit.ac.jp>
2021-09-14 11:13:16 +0900

    タイトル変更機能改良

--- js/SEFrame.js
+++ js/SEFrame.js
@@ -1,18 +1,18 @@
 function toOITHP(){
     //ここで親要素の#titleと#linkを書き換え
-    document.getElementById("title").innerHTML="大阪工業大学";
+    parent.document.getElementById("title").innerHTML="大阪工業大学";
     parent.document.getElementById("link").innerHTML="https://www.oit.ac.jp/";
 }
 
 function toISHP(){
     //ここで親要素の#titleと#linkを書き換え
-    document.getElementById("title").innerHTML="情報セキュリティ研究室";
+    parent.document.getElementById("title").innerHTML="情報セキュリティ研究室";
     parent.document.getElementById("link").innerHTML="https://www.oit.ac.jp/laboratory/room/254";
 }
 
 function toRANSOMHP(){
     //ここで親要素の#titleと#linkを書き換え
-    document.getElementById("title").innerHTML="ランサムウェア";
+    parent.document.getElementById("title").innerHTML="ランサムウェア";
     parent.document.getElementById("link").innerHTML="https://oitfukuzawaken2021.github.io/RansomwareExp/html/ransom.html";
     parent.document.getElementById("pop-up").checked = true;
 }
