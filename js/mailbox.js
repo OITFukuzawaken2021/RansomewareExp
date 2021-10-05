04153882ce278982eb5ffb126ae799ae3441d838
n19078 <e1n19078@st.oit.ac.jp>
2021-10-05 17:53:02 +0900

    次こそ更新できますように

--- js/mailbox.js
+++ js/mailbox.js
@@ -1,7 +1,64 @@
-function pushmail(){
+function pushmail1(){
     document.getElementById("mail1").style.display="block";
 }
 
-function Closemail(){
+function Closemail1(){
     document.getElementById("mail1").style.display="none";
+}
+
+function pushmail2(){
+    document.getElementById("mail2").style.display="block";
+}
+function Closemail2(){
+    document.getElementById("mail2").style.display="none";
+}
+function pushmail3(){
+    document.getElementById("mail3").style.display="block";
+}
+function Closemail3(){
+    document.getElementById("mail3").style.display="none";
+}
+function pushmail4(){
+    document.getElementById("mail4").style.display="block";
+}
+function Closemail4(){
+    document.getElementById("mail4").style.display="none";
+}
+function pushmail5(){
+    document.getElementById("mail5").style.display="block";
+}
+function Closemail5(){
+    document.getElementById("mail5").style.display="none";
+}
+function pushmail6(){
+    document.getElementById("mail6").style.display="block";
+}
+function Closemail6(){
+    document.getElementById("mail6").style.display="none";
+}
+function pushmail7(){
+    document.getElementById("mail7").style.display="block";
+}
+function Closemail7(){
+    document.getElementById("mail7").style.display="none";
+}
+function pushmail8(){
+    document.getElementById("mail8").style.display="block";
+}
+function Closemail8(){
+    document.getElementById("mail8").style.display="none";
+}
+function pushmail9(){
+    document.getElementById("mail9").style.display="block";
+}
+function Closemail9(){
+    document.getElementById("mail9").style.display="none";
+}
+
+
+function pushbutton(){
+    document.getElementById("botan").style.display="block";
+}
+function Closebutton(){
+    document.getElementById("botan").style.display="none";
 }
